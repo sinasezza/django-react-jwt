1e5f4e8c65009212806cd53e818a7b7310f90388
sinasezza <s.e.sezza121@gmail.com>
2024-03-21 16:31:15 +0330

    feat(settings): django rest framework and simple jwt added to project and configured in settings.

--- base/api/apis.py
+++ base/api/apis.py
@@ -1,11 +1,33 @@
-from django.http import JsonResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import rest_framework.status as rest_statuses
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
-def get_routes_api(request):
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    
+
+@api_view(http_method_names=['GET',])
+def get_routes_api(request: HttpRequest) -> HttpResponse:
     routes = [
         '/api/token',
         '/api/token/refresh/',
         'api/user/register/',
         'api/user/login/'
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(data=routes, status=rest_statuses.HTTP_200_OK)
--- base/api/urls.py
+++ base/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    # TokenObtainPairView,
+    TokenRefreshView,
+)
 from . import apis
 
 app_name = 'base-apis'
 
 urlpatterns = [
     path('', apis.get_routes_api, name='get-routes'),
-    
+    path('token/', apis.MyTokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
